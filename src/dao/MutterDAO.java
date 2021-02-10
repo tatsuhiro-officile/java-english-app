4283a5e44ce08e8e975d9e6b81253c5aa58b5195
tatsuhiro <tatsuhiro.officile@gmail.com>
2021-02-10 21:12:16 +0900

    commit

--- src/dao/MutterDAO.java
+++ src/dao/MutterDAO.java
@@ -1,5 +1,7 @@
 package dao;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -12,19 +14,25 @@ import model.Mutter;
 
 public class MutterDAO {
 
-  private final String DRIVER_NAME = "com.mysql.cj.jdbc.Driver";
-  private final String JDBC_URL =
-      "mysql://b32ab6bf45681a:b7722d39@us-cdbr-east-03.cleardb.com/heroku_9753101ada075c9?reconnect=true";
-  private final String DB_USER = "root";
-  private final String DB_PASS = "fdg65*Fkkdw";
+    private static Connection getConnection() throws URISyntaxException, SQLException {
+        // heroku configに設定されている値を取得。
+    URI dbUri = new URI(System.getenv("CLEARDB_DATABASE_URL"));
+    // :をデリミタとして必要な情報を抜き取る。
+    String username = dbUri.getUserInfo().split(":")[0];
+    String password = dbUri.getUserInfo().split(":")[1];
+    // JDBC用のURLを生成。
+    String dbUrl = "jdbc:mysql://" + dbUri.getHost() + dbUri.getPath();
+
+    return DriverManager.getConnection(dbUrl, username, password);
+}
+
+
 
   public List<Mutter> findAll() {
-    Connection conn = null;
     List<Mutter> mutterList = new ArrayList<Mutter>();
     try {
-      Class.forName(DRIVER_NAME);
-      conn = DriverManager.getConnection(
-          JDBC_URL, DB_USER, DB_PASS);
+      Class.forName("com.mysql.cj.jdbc.Driver");
+     try (Connection conn =getConnection()){
 
       // SELECT文の準備
       String sql =
@@ -42,60 +50,17 @@ public class MutterDAO {
         Mutter mutter = new Mutter(id, userName, text);
         mutterList.add(mutter);
       }
-    } catch (SQLException e) {
-      e.printStackTrace();
-      return null;
-    } catch (ClassNotFoundException e) {
-      e.printStackTrace();
-      return null;
-    } finally {
-      // データベース切断
-      if (conn != null) {
-        try {
-          conn.close();
-        } catch (SQLException e) {
-          e.printStackTrace();
-          return null;
-        }
-      }
-    }
-    return mutterList;
-  }
-
-  public boolean create(Mutter mutter) {
-    Connection conn = null;
-    try {
-      // データベースへ接続
-      conn = DriverManager.getConnection(
-          JDBC_URL, DB_USER, DB_PASS);
-
-      // INSERT文の準備(idは自動連番なので指定しなくてよい）
-      String sql = "INSERT INTO MUTTER(NAME, TEXT) VALUES(?, ?)";
-      PreparedStatement pStmt = conn.prepareStatement(sql);
-      // INSERT文中の「?」に使用する値を設定しSQLを完成
-      pStmt.setString(1, mutter.getUserName());
-      pStmt.setString(2, mutter.getText());
-
-      // INSERT文を実行
-      int result = pStmt.executeUpdate();
-
-      if (result != 1) {
-        return false;
-      }
-    } catch (SQLException e) {
-      e.printStackTrace();
-      return false;
-    } finally {
-
-      // データベース切断
-      if (conn != null) {
-        try {
-          conn.close();
-        } catch (SQLException e) {
-          e.printStackTrace();
-        }
-      }
-    }
-    return true;
-  }
+     } catch (URISyntaxException e) {
+         e.printStackTrace();
+         return null;
+     } catch (SQLException e) {
+         e.printStackTrace();
+         return null;
+     }
+ } catch (ClassNotFoundException e1) {
+     e1.printStackTrace();
+     return null;
+ }
+ return null;
+}
 }
