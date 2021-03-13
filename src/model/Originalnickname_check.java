792ff0c070d99348a513a11a22b44bc2cc10e8a3
tatsuhiro <tatsuhiro.officile@gmail.com>
2021-03-13 12:58:46 +0900

    commit

--- src/model/Originalnickname_check.java
+++ src/model/Originalnickname_check.java
@@ -7,6 +7,7 @@ public class Originalnickname_check{
   public String originalnicikname_check(Register register) { // 引数を1つに変更
     UserDAO dao = new UserDAO();
     String checkresult= dao.findnickname(register);
+    System.out.println("オリジナルcheckresult"+checkresult);
 
     return checkresult;
   }
